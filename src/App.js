3226bbbfb89278661e2bec9bbd482183273dda74
Filip Johansen <hello@filipjohansen.com>
2022-03-31 19:49:36 +0200

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,22 @@
-import logo from './logo.svg';
+
 import './App.css';
+import { useState } from 'react';
+
 
 function App() {
+  const [text, setText] = useState('');
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      
+      <textarea value={text} onChange={(e) => setText(e.target.value)}>
+
+      </textarea>
+      <p>{text.toUpperCase()}</p>
+      
+       
+        
     </div>
+    
   );
 }
 
