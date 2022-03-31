d1c58a08ac216100d2e384e3a59157e855d155d7
Filip Johansen <hello@filipjohansen.com>
2022-03-31 20:17:56 +0200

    updated colors etc

--- src/App.js
+++ src/App.js
@@ -10,8 +10,9 @@ function App() {
   const [text, setText] = useState('');
   return (
     <div className="App">
+      <h1>Enkoder</h1>
       
-      <textarea value={text} onChange={(e) => setText(e.target.value)}>
+      <textarea value={text} onChange={(e) => setText(e.target.value)} placeholder="Skriv inn tekst her, også genereres det automatisk en enkodet versjon under">
 
       </textarea>
       <p>{text.toUpperCase()}</p>
